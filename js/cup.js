6fc1d8648255db67331dcde04dc641f9b55a0c36
gzero1016 <Gzero1016@gmail.com>
2023-08-06 21:27:07 +0900

    feat: js ing

---
+++ js/cup.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+    const cupImg = document.querySelector(".img-border img");
+    cupImg.src = "./images/cup1.jpg";
+    cupImg.onclick = cupImgOnClickHandle;
+
+    // document.onclick = () => {
+    //     if(cupImg.src.includes("cup1")) {
+    //         cupImg.src = "./images/cup2.jpg";
+    //     }else {
+    //         cupImg.src = "./images/cup1.jpg";
+    //     }
+    // }
+    
+}
+
+const cupImgOnClickHandle = (e) => {
+    console.log(e.target);
+    if(e.target.src.includes("cup1")) {
+        e.target.src = "./images/cup2.jpg";
+    }else {
+        e.target.src = "./images/cup1.jpg";
+    }
+}
