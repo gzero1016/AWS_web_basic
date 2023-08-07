649cbc584ce525eecf1e8c12a9f523b313085ae8
gzero1016 <Gzero1016@gmail.com>
2023-08-07 14:06:54 +0900

    feat: 기초 마무리

--- js/cup.js
+++ js/cup.js
@@ -1,8 +1,11 @@
 window.onload = () => {
     const cupImg = document.querySelector(".img-border img");
+    // 이미지 주소
     cupImg.src = "./images/cup1.jpg";
+    // 클릭했을경우 cupImgOnClickHandle 실행
     cupImg.onclick = cupImgOnClickHandle;
 
+    // cupImg.onclick 에 한번에 작성했을 경우
     // document.onclick = () => {
     //     if(cupImg.src.includes("cup1")) {
     //         cupImg.src = "./images/cup2.jpg";
@@ -13,8 +16,10 @@ window.onload = () => {
     
 }
 
+// e : 이벤트 객체
 const cupImgOnClickHandle = (e) => {
     console.log(e.target);
+    // includes : 해당요소가 포함되어있는지 확인하는 거
     if(e.target.src.includes("cup1")) {
         e.target.src = "./images/cup2.jpg";
     }else {
